d400d8744b0e91a347931cf0eb8af10302090bf8
Xuan Li <xli100@stevens.edu>
2016-10-23 20:13:08 -0400

    Create mongoCollections.js

---
+++ conifg/mongoCollections.js
@@ -0,0 +1,30 @@
+
+"use strict";
+const dbConnection = require("./mongoConnection");
+/*
+ Created by lx on 2016/10/17
+ Name: Xuan Li
+ CWID:10409939
+ Email: xli100@stevens.edu
+ */
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+let getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return () => {
+        if (!_col) {
+            _col = dbConnection().then(db => {
+                return db.collection(collection);
+            });
+        }
+
+        return _col;
+    }
+}
+
+/* Now, you can list your collections here: */
+module.exports = {
+    recipe : getCollectionFn("recipe")
+};
+
