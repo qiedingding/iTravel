9dbb30db03025c03822534a305be82c6de873ba2
Ianluckier <yli152@stevens.edu>
2016-12-01 22:29:32 -0500

    Add files via upload

---
+++ routes/food.js
@@ -0,0 +1,69 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const foodData = data.food;
+
+router.get("/", (req, res) => {
+	foodData.getAllFood().then((foodList) => {
+		res.json(foodList);
+	}).catch((e) => {
+		res.status(500).json({error: e});
+	});
+});
+
+router.get("/foodId/:id", (req, res) => {
+	foodData.getFoodById(req.params.id).then((food) => {
+		res.json(food);
+	}).catch(() => {
+		res.status(404).json({error: "Food not found!"});
+	});
+});
+
+router.get("/foodName/:name", (req, res) => {
+	foodData.getFoodByName(req.params.name).then((food) => {
+		res.json(food);
+	}).catch(() => {
+		res.status(404).json({error: "Food not found!"});
+	});
+});
+
+router.post("/", (req, res) => {
+	let foodPostData = req.body;
+	foodData.addFood(foodPostData.name, foodPostData.description, foodPostData.location, foodPostData.address, foodPostData.price, foodPostData.closingTime, foodPostData.phone, foodPostData.website, foodPostData.mainImage, foodPostData.type, foodPostData.cityId).then((newFood) => {
+		res.json(newFood);
+	}).catch((e) => {
+		res.status(500).json({error: e});
+	});
+});
+
+router.put("/:id", (req, res) => {
+	let updatedData = req.body;
+	let getFood = foodData.getFoodById(req.params.id);
+
+	getFood.then(() => {
+		return foodData.updateFood(req.params.id, updatedData).then((updatedFood) => {
+			res.json(updatedFood);
+		}).catch((e) => {
+			res.status(500).json({error: e});
+		});
+	}).catch(() => {
+			res.status(404).json({error: "Food not found!"});
+	});
+});
+
+router.delete("/:id", (req, res) => {
+	let getFood = foodData.getFoodById(req.params.id);
+
+	getFood.then(() => {
+		return foodData.removeFood(req.params.id).then(() => {
+			res.sendStatus(200);
+		}).catch((e) => {
+			res.status(500).json({error: e});
+		});
+	}).catch(() => {
+		res.status(404).json({error: "Food not found!"});
+	});
+});
+
+module.exports = router;
---
+++ routes/site.js
@@ -0,0 +1,69 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const siteData = data.site;
+
+router.get("/", (req, res) => {
+	siteData.getAllSites().then((siteList) => {
+		res.json(siteList);
+	}).catch((e) => {
+		res.status(500).json({error: e});
+	});
+});
+
+router.get("/siteId/:id", (req, res) => {
+	siteData.getSiteById(req.params.id).then((site) => {
+		res.json(site);
+	}).catch(() => {
+		res.status(404).json({error: "Site not found!"});
+	});
+});
+
+router.get("/siteName/:name", (req, res) => {
+	siteData.getSiteByName(req.params.name).then((site) => {
+		res.json(site);
+	}).catch(() => {
+		res.status(404).json({error: "Site not found!"});
+	});
+});
+
+router.post("/", (req, res) => {
+	let sitePostData = req.body;
+	siteData.addSite(sitePostData.name, sitePostData.location, sitePostData.address, sitePostData.commute, sitePostData.price, sitePostData.closingTime, sitePostData.phone, sitePostData.website, sitePostData.description, sitePostData.mainImage, sitePostData.type, sitePostData.tips, sitePostData.tag, sitePostData.cityId).then((newSite) => {
+		res.json(newSite);
+	}).catch((e) => {
+		res.status(500).json({error: e});
+	});
+});
+
+router.put("/:id", (req, res) => {
+	let updatedData = req.body;
+	let getSite = siteData.getSiteById(req.params.id);
+
+	getSite.then(() => {
+		return siteData.updateSite(req.params.id, updatedData).then((updatedSite) => {
+			res.json(updatedSite);
+		}).catch((e) => {
+			res.status(500).json({error: e});
+		});
+	}).catch(() => {
+			res.status(404).json({error: "Site not found!"});
+	});
+});
+
+router.delete("/:id", (req, res) => {
+	let getSite = siteData.getSiteById(req.params.id);
+
+	getSite.then(() => {
+		return siteData.removeSite(req.params.id).then(() => {
+			res.sendStatus(200);
+		}).catch((e) => {
+			res.status(500).json({error: e});
+		});
+	}).catch(() => {
+		res.status(404).json({error: "Site not found!"});
+	});
+});
+
+module.exports = router;
